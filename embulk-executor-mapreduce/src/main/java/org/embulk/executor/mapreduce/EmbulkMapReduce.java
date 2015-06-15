9bd2d5864efadc86744c3d49a238fee5c1eb4395
Muga Nishizawa <muga.nishizawa@gmail.com>
2015-06-14 22:57:24 -0700

    Revert "fixed IOException handling when attempt state files are opened." because we should fix it with retry executor.
    This reverts commit 0213c28d2d849435882019e03448cade40872e1a.

--- embulk-executor-mapreduce/src/main/java/org/embulk/executor/mapreduce/EmbulkMapReduce.java
+++ embulk-executor-mapreduce/src/main/java/org/embulk/executor/mapreduce/EmbulkMapReduce.java
@@ -1,6 +1,5 @@
 package org.embulk.executor.mapreduce;
 
-import java.io.EOFException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -146,13 +145,8 @@ public class EmbulkMapReduce
             Path stateDir, TaskAttemptID id, ModelManager modelManager) throws IOException
     {
         Path path = new Path(stateDir, id.toString());
-
-        try (FSDataInputStream in = path.getFileSystem(config).open(path)) { // throw IOException
-            return AttemptState.readFrom(in, modelManager); // throw EOFException
-        } catch (EOFException e) {
-            throw e;
-        } catch (IOException e) {
-            throw new EOFException("Cannot open the attempt state file.");
+        try (FSDataInputStream in = path.getFileSystem(config).open(path)) {
+            return AttemptState.readFrom(in, modelManager);
         }
     }
 
